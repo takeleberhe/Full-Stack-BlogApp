ba0b053c0a633dec71da41725c83c7195c8dfd6c
takeleberhe <77755440+takeleberhe@users.noreply.github.com>
2024-06-29 19:17:23 +0200

    Adding form validation using react-hook-form

--- Front-end/src/Pages/Login.jsx
+++ Front-end/src/Pages/Login.jsx
@@ -1,48 +1,64 @@
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { SignIn } from "../Redux/AuthReducer/AuthSlice";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const { register, handleSubmit } = useForm({ mode: "all" });
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleSingIn = () => {
-    dispatch(SignIn({ email, password })).then(() => navigate("/"));
+  const handleSingIn = async (data) => {
+    const payload = data;
+    dispatch(SignIn(payload)).then(() => navigate("/"));
   };
 
   return (
     <div>
       <div className="flex flex-col justify-center items-center">
-        <div className="w-80 bg-gray-500 rounded-md m-8 p-4 shadow-lg">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="p-2 m-2 outline-none rounded-md w-[280px] "
-            placeholder="enter email"
-          ></input>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="p-2 m-2 outline-none rounded-md w-[280px]"
-            placeholder="enter password"
-          ></input>
-          <button
-            className="p-2 m-4 ml-10 w-[200px] rounded-2xl bg-blue-500"
-            type="submit"
-            onClick={handleSingIn}
-          >
-            Submit
-          </button>
-        </div>
+        <form onSubmit={handleSubmit(handleSingIn)}>
+          <div className="w-80 bg-gray-500 rounded-md m-8 p-4 shadow-lg">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              {...register("email", {
+                required: "email required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "email must be valid",
+                },
+              })}
+              placeholder="enter email"
+              className="p-2 m-2 outline-none rounded-md w-[280px] "
+            ></input>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              {...register("password", {
+                required: "password required",
+                /* To check a password between 7 to 15 characters which contain at 
+               least one numeric digit and a special character */
+                pattern: {
+                  value:
+                    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/,
+                  message:
+                    "password must be 7-15characters and must include one numeric and one special character",
+                },
+              })}
+              className="p-2 m-2 outline-none rounded-md w-[280px]"
+              placeholder="enter password"
+            ></input>
+            <button
+              className="p-2 m-4 ml-10 w-[200px] rounded-2xl bg-blue-500"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- Front-end/src/Pages/SignUp.jsx
+++ Front-end/src/Pages/SignUp.jsx
@@ -1,28 +1,39 @@
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { signUp } from "../Redux/AuthReducer/AuthSlice";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 const SignUp = () => {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const { register, handleSubmit } = useForm({ mode: "all" });
   const navigate = useNavigate();
-
   const dispatch = useDispatch();
 
-  const handleSignUp = () => {
-    dispatch(signUp({ name, email, password })).then(() => navigate("/login"));
+  const handleSignUp = (data) => {
+    const payload = data;
+    dispatch(signUp(payload)).then(() => navigate("/login"));
   };
   return (
     <div>
       <div className=" flex flex-col justify-center items-center">
-        <div className="bg-gray-500 w-[300px] rounded-md p-4 m-6 shadow-lg">
+        <form
+          className="bg-gray-500 w-[300px] rounded-md p-4 m-6 shadow-lg"
+          onSubmit={handleSubmit(handleSignUp)}
+        >
           <label htmlFor="name">Name:</label>
           <input
             type="text"
             name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            {...register("name", {
+              required: "username required!",
+              minLength: {
+                value: 3,
+                message: "username must be at least 3 characters!",
+              },
+
+              maxLength: {
+                value: 20,
+                message: "username must be at most 20 characters!",
+              },
+            })}
             className="p-2 m-2 outline-none rounded-md w-[250px]"
             placeholder="enter your name"
           ></input>
@@ -30,8 +41,13 @@ const SignUp = () => {
           <input
             type="email"
             name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            {...register("email", {
+              required: "email required",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "email must be valid",
+              },
+            })}
             className="p-2 m-2 outline-none rounded-md w-[250px]"
             placeholder="eneter your email"
           ></input>
@@ -39,19 +55,26 @@ const SignUp = () => {
           <input
             type="password"
             name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            {...register("password", {
+              required: "password required",
+              /* To check a password between 7 to 15 characters which contain at 
+                 least one numeric digit and a special character */
+              pattern: {
+                value: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/,
+                message:
+                  "password must be 7-15characters and must include one numeric and one special character",
+              },
+            })}
             className="p-2 m-2 rounded-md outline-none w-[250px]"
             placeholder="enter password"
           ></input>
           <button
             type="submit"
             className="w-[200px] rounded-2xl m-3 ml-8 p-2 bg-blue-500 outline-none"
-            onClick={handleSignUp}
           >
             Submit
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
